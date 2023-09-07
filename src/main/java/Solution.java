dce792bc98cc86c7d747a34bcb46dad99e066426
alek <wait82985@gmail.com>
2023-09-07 12:24:43 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,41 @@
 public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode preHead = new ListNode();
+        preHead.next = head;
 
-        return null;
-    }
+        ListNode leftNode = head;
+        ListNode preLeftNode = null;
+        int counter = 1;
+        while (counter != left){
+            preLeftNode = leftNode;
+            leftNode = leftNode.next;
+            counter++;
+        }
+        int n = right - left + 1;
+        ListNode previous = null;
+        ListNode current = leftNode;
+        ListNode next = null;
+
+        ListNode nodeToSaveTheLinkToTail = null;
+        while (n != 0){
+            nodeToSaveTheLinkToTail = current.next;
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+            n--;
+        }
+        if(preLeftNode != null) {
+            preLeftNode.next = previous;
+        }else preHead.next = previous;
+
+        while (previous.next != null){
+            previous = previous.next;
+        }
+        previous.next = nodeToSaveTheLinkToTail;
+
+
+        return preHead.next;
+
+   }
 }
